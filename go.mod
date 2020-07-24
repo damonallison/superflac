73294ccc8659a5c253c2a8528154ac061df173fa
Damon Allison <damon@damonallison.com>
2020-07-24 07:55:37 -0500

    Adds concurrency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/damonallison/superflac
 
 go 1.14
+
+require github.com/gammazero/workerpool v1.0.0
--- main.go
+++ main.go
@@ -46,159 +46,137 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
+	"runtime"
 	"strings"
+
+	"github.com/gammazero/workerpool"
 )
 
-var qFlag string
+type options struct {
+	quality PresetType
+	rootDir string
+}
 
 func main() {
-
-	flag.StringVar(&qFlag, "quality", string(PresetTypeExtreme), "Specifies the lame preset type to use. Possible values are 'standard', 'extreme', and 'insane'")
-	flag.Parse()
-
-	if len(flag.Args()) == 0 {
-		fmt.Printf("Usage: superflac -quality [standard|extreme|insane] /path/to/dir")
+	opts, err := parseArgs()
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	dir := flag.Args()[0]
-
 	if !verifyDeps() {
 		log.Println("One or more dependencies count not be found. Install those and come back later.")
 		os.Exit(1)
 	}
-	if !exists(dir) {
-		log.Printf("'%s' does not exist", dir)
+	log.Printf("superflac is starting. dir = (%s) quality = (%s)\n", opts.rootDir, opts.quality)
+
+	candidates := make([]string, 0)
+	err = filepath.Walk(opts.rootDir, func(fileName string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		cleanPath := path.Clean(fileName)
+		if !info.IsDir() && strings.EqualFold(path.Ext(cleanPath), ".flac") {
+			candidates = append(candidates, cleanPath)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("superflac failed - %v\n", err)
 		os.Exit(1)
 	}
-
-	log.Printf("Superflac is starting. dir = (%s) quality = (%s)", dir, qFlag)
-
-	if err := filepath.Walk(dir, walkFunc); err != nil {
-		log.Fatalf("Superflac failed with err (%v)", err)
+	if err := encodeFlacToMp3(candidates, opts); err != nil {
+		fmt.Printf("superflac failed - %v\n", err)
+		os.Exit(1)
 	}
 }
 
-// walkFunc is called for each path. We'll use this to determine
-// what the file extension is. If a flac file is encountered,
-// we'll encode it.
-func walkFunc(fileName string, info os.FileInfo, err error) error {
+func parseArgs() (options, error) {
+	q := flag.String(
+		"quality",
+		string(PresetTypeExtreme),
+		"Specifies the lame preset type to use. Possible values are 'standard', 'extreme', and 'insane'")
+	flag.Parse()
 
-	if err != nil {
-		return err
+	if len(flag.Args()) == 0 {
+		return options{}, fmt.Errorf("directory not specified. usage: superflac -quality [standard|extreme|insane] /path/to/encode")
 	}
-	if info.IsDir() {
-		return nil
+	dir := flag.Args()[0]
+	if !isDir(dir) {
+		return options{}, fmt.Errorf("directory does not exist: %s", dir)
 	}
-	cleanPath := path.Clean(fileName)
-	log.Printf("Base == \"%s\" Dir == \"%s\" Ext == \"%s\"\n",
-		path.Base(cleanPath),
-		path.Dir(cleanPath),
-		path.Ext(cleanPath))
+	return options{
+		quality: PresetType(*q),
+		rootDir: flag.Args()[0],
+	}, nil
+}
 
-	if path.Ext(cleanPath) == ".flac" {
-		if !encodeFlacToMp3(cleanPath) {
-			log.Printf("Encode failed for file at %s\n", cleanPath)
-		} else {
-			log.Printf("Encode success %s\n", cleanPath)
-		}
-	} else {
-		log.Printf("skipping non-flac file at path == \"%s\"\n", cleanPath)
+func encodeFlacToMp3(candidates []string, opts options) error {
+	wg := workerpool.New(runtime.NumCPU())
+
+	for _, candidate := range candidates {
+		wg.Submit(func() {
+			if ok, err := isFlacFileValid(candidate); !ok {
+				log.Printf("unable to encode file. `flac` says this file is invalid : %s error : %v\n", candidate, err)
+				return
+			}
+
+			// create MP3 directory
+			outDir := filepath.Join(path.Dir(candidate), "mp3")
+			outBase := filepath.Base(candidate)
+			outPath := filepath.Join(outDir, strings.TrimSuffix(outBase, filepath.Ext(outBase)))
+			outPath = outPath + ".mp3"
+
+			if !ensureDir(outDir) {
+				log.Printf("Unable to create output directory at %s\n", outDir)
+				return
+			}
+
+			if err := os.RemoveAll(outPath); err != nil {
+				log.Printf("Unable to remove existing path at %s Error:%v\n", outPath, err)
+				return
+			}
+
+			// OK : we are ready to encode to MP3
+
+			// Before we start the decode/encode, determine if the flac file is valid
+			cmdFlac := exec.Command("flac", "--decode", "--stdout", "--totally-silent", candidate)
+			// --quiet
+			cmdLame := exec.Command("lame", "--preset", string(opts.quality), "--quiet", "-", outPath)
+			printCmd(cmdFlac)
+			printCmd(cmdLame)
+
+			cmdLame.Stdin, _ = cmdFlac.StdoutPipe()
+			cmdLame.Stdout = os.Stdout
+			cmdLame.Stderr = os.Stderr
+			_ = cmdLame.Start()
+			_ = cmdFlac.Start()
+			_ = cmdLame.Wait()
+
+			// TODO - @dra - check the return codes from either process. If
+			// either fails, error out.
+		})
 	}
+	wg.StopWait()
+	// @dra - better error handling?
 	return nil
-}
-
-// convertFlacToMp3 will convert `file` and return a boolean
-// indicating if the file was successfully encoded and the path
-// where it was encoded to.
-func encodeFlacToMp3(inPath string) bool {
-
-	if !exists(inPath) {
-		log.Printf("Unable to encode file. path does not exist : %s\n", inPath)
-		return false
-	}
-
-	// TODO : better way to determine if a file exists than .Stat()?
-	fi, err := os.Stat(inPath)
-	if err != nil {
-		log.Printf("Unable to enode file. Error == %v", err)
-		return false
-	}
-
-	if fi.IsDir() {
-		log.Printf("Unable to encode file. path is not a valid flac file : %s\n", inPath)
-		return false
-	}
-
-	if ok, err := isFlacFileValid(inPath); !ok {
-		log.Printf("Unable to encode file. `flac` says this file is invalid : %s Error : %v\n",
-			inPath, err)
-		return false
-	}
-
-	log.Printf("Attempting to encode flac file at \"%s\"", inPath)
-
-	// create MP3 directory
-	outDir := filepath.Join(path.Dir(inPath), "mp3")
-	outBase := filepath.Base(inPath)
-	outPath := filepath.Join(outDir, strings.TrimSuffix(outBase, filepath.Ext(outBase)))
-	outPath = outPath + ".mp3"
-
-	log.Printf("output mp3 filename = %s", outPath)
-
-	if !ensureDir(outDir) {
-		log.Printf("Unable to create output directory at %s\n", outDir)
-		return false
-	}
-
-	if err := os.RemoveAll(outPath); err != nil {
-		log.Printf("Unable to remove existing path at %s Error:%v\n", outPath, err)
-		return false
-	}
-
-	// OK : we are ready to encode to MP3
-
-	// Before we start the decode/encode, determine if the flac file is valid
-	cmdFlac := exec.Command("flac", "--decode", "--stdout", "--totally-silent", inPath)
-	// --quiet
-	cmdLame := exec.Command("lame", "--preset", qFlag, "--quiet", "-", outPath)
-	printCmd(cmdFlac)
-	printCmd(cmdLame)
-
-	cmdLame.Stdin, _ = cmdFlac.StdoutPipe()
-	cmdLame.Stdout = os.Stdout
-	cmdLame.Stderr = os.Stderr
-	_ = cmdLame.Start()
-	_ = cmdFlac.Start()
-	_ = cmdLame.Wait()
-
-	// TODO : check the return codes from either process.
-	//        if either fails, error out.
-	return true
 }
 
 // ensureDir will create a directory at `fileName` if one
 // does not already exist. If a file already exists, or if
 // we are not able to create a direcotry, returns false
 func ensureDir(dirPath string) bool {
-
-	if !exists(dirPath) {
-		log.Printf("Making directory at (%v)", dirPath)
+	if !isDir(dirPath) {
 		err := os.MkdirAll(dirPath, 0777)
 		return err == nil
+	} else {
+		return true
 	}
-
-	// Exists - make sure we have a file here.
-	fi, err := os.Stat(dirPath)
-	if err != nil {
-		log.Printf("Unable to stat file at %v", dirPath)
-		return false
-	}
-	return fi.IsDir()
 }
 
-func exists(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
+func isDir(path string) bool {
+	stat, err := os.Stat(path)
+	if err != nil {
 		return false
 	}
-	return true
+	return stat.IsDir()
 }
